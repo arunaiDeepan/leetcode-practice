fc4d10a0fa0183b4871f89ba332a48398a60b8d2
Arunai Deepan <64122294+arunaiDeepan@users.noreply.github.com>
2024-12-02 00:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (16.77%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.go
@@ -0,0 +1,10 @@
+func checkIfExist(arr []int) bool {
+    seen := make(map[int]bool)
+    for _, num := range arr {
+        if seen[num*2] || (num%2 == 0 && seen[num/2]) {
+            return true
+        }
+        seen[num] = true
+    }
+    return false
+}
