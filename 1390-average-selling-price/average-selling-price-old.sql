7d9002d33b9ce298c985eec5f3d67e546bafb7ff
Arunai <64122294+arunaiDeepan@users.noreply.github.com>
2026-03-11 19:32:34 +0530

    Time: 236 ms (20.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price-old.sql
@@ -0,0 +1,16 @@
+SELECT 
+    p.product_id,
+    ROUND(
+        COALESCE(
+            SUM(p.price * u.units)::numeric /
+            NULLIF(SUM(u.units),0),
+            0
+        ), 2
+    ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u 
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
+
+-- Time: 236 ms (20.98%) | Memory: 0B (100.00%) - LeetSync 
