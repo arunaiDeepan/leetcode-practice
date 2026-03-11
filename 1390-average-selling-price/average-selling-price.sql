6df2484cc34ce4125834d7afc18f9bd93f044e7e
Arunai <64122294+arunaiDeepan@users.noreply.github.com>
2026-03-11 19:20:39 +0530

    Time: 203 ms (59.18%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,15 +1,21 @@
 SELECT 
-    p.product_id,
-    ROUND
-    (
+    product_id,
+    ROUND(
         COALESCE(
-            SUM(p.price * u.units)::numeric /
-            NULLIF(SUM(u.units),0),
+            (revenue)::numeric / 
+            NULLIF(no_units,0),
             0
-        ), 2
+        ),
+        2
     ) AS average_price
-FROM Prices p
-LEFT JOIN UnitsSold u 
-    ON p.product_id = u.product_id
-    AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id;
+FROM (
+    SELECT 
+        p.product_id,
+        SUM(p.price * u.units) AS revenue,
+        SUM(u.units) AS no_units
+    FROM Prices p
+    LEFT JOIN UnitsSold u 
+        ON p.product_id = u.product_id
+        AND u.purchase_date BETWEEN p.start_date AND p.end_date
+    GROUP BY p.product_id
+) sub_t;
